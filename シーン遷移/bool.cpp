4446978d324d8c50d88d08b6e258b23adf5621a9
流体初心者 <64288912+fluid-love@users.noreply.github.com>
2021-03-25 21:48:25 +0900

    Create bool.cpp

---
+++ シーン遷移/bool.cpp
@@ -0,0 +1,24 @@
+bool title = false;
+bool battle = false;
+bool result = false;
+
+int main() {
+
+	title = true;
+
+	//描写ループ　
+	while (true) {
+
+		if (title) {
+			//タイトルの処理
+		}
+		else if (battle) {
+			//バトルの処理
+		}
+		else {
+			//リザルトの処理
+		}
+
+	}
+
+}
